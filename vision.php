ece32747e944c02f9252b8531333e070f54eccb5
saturnino2022 <121139331+saturnino2022@users.noreply.github.com>
2022-12-22 09:47:08 -0600

    Add files via upload

---
+++ vision.php
@@ -0,0 +1,171 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/loading.css">
+    <style>
+       #foot{
+    background: #DEBAFC;
+}
+     </style>
+
+     <title>Visiòn</title>
+  </head>
+  <body>
+     <div class="contenedor_loader">
+        <div class="loader" >
+            <h6 id="carga">Cargando...</h6>            
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+  <div class="container-fluid">
+          <nav class="navbar navbar-light ">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">
+      <img src="imagenes/editarLog.png" alt="" width="90" height="55" class="d-inline-block align-text-top">     
+    </a>
+  </div>
+</nav>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-6 mb-lg-0">
+
+                   <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" class="text-primary" href="index.php"><h6>Inicio</h6></a>
+                   </li>
+                   <li class="nav-item">
+                    <a class="nav-link active" class="link-primary" aria-current="page" href="Mision.php"><h6>Misión</h6></a>
+                   </li>
+                   <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" class="link-primary" href="vision.php"><h6>Visión</h6></a>
+                   </li>
+                  
+      </ul>
+    </div>
+   
+  </div>
+  <!--  --> 
+</nav>
+
+   
+<br>
+    
+   <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 mt-5">
+            <div class="card">
+              <div class="card-header">
+                <h4>Visión</h4>                
+              </div>
+              
+            </div>
+          </div>
+          <div class="col-md-12-center mt-4">
+            <div class="card">
+              <div class="row">
+                 <div class="col-md-12 ">
+                  <img src="imagenes/visionimg.png" width="400" height="400"  class="img-fluid" >
+                  </div>
+                    <div class="col-md-12" >
+                      <div class="card-body bg-secondary  bg-opacity-10">
+                        <p>
+                          Ser una macro empresa líder a nivel nacional, sobrepasando las perspectivas de nuestros clientes con productos de alta calidad y el continuo crecimiento de nuestra macro empresa.
+                        </p>
+                      </div> 
+                               
+                    </div>
+              </div>              
+            </div>
+          </div>
+     
+        </div>
+      </div>
+    </section>
+    <br>
+
+ <footer class=" text-black pt-5 pb-4" id="foot" >
+ 
+  <div class="container text-center text-md-left">
+
+    <div class="row text-center text-md-left">
+
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold text-dark">Empresa</h6>
+          <p>
+        <a  class="text-purple" style="text-decoration: none;"> Herrajes Andrea</a>
+      </p>
+        
+      </div>
+
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold text-dark">Información</h6>
+      <p>
+        <a href="SobreNosotros.php" class="text-purple" style="text-decoration: none;"> Sobre nosotros</a>
+      </p>
+      <p>
+        <a href="maps.php" class="text-purple" style="text-decoration: none;"> Sucursales</a>
+      </p>
+      </div>
+
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold text-dark" >Atención a clientes</h6>
+        <p>
+        
+          <img src="img/what.png" alt="" width="17" height="17" class="d-inline-block align-text-top">
+      <font size=2 class="d-inline-block align-text-top" color="Purple">55-25-48-61-73</font>
+         
+         </a>
+        </p>
+         <p>
+           
+          <img src="img/tel.png" alt="" width="17" height="17" class="d-inline-block align-text-top">
+      <font size=2 class="d-inline-block align-text-top" color="Purple">55-55-42-80-63</font>
+         
+         </a>
+        </p>
+      </div>
+      
+    </div>
+
+    <hr class="mb-4">
+
+    <div class="row align-items-center">
+
+      <div class="col-md-7 col-lg-8">
+        <p> Copyright ©2021 Todos los derechos reservados por:
+          <a  style="text-decoration: none;">
+            <strong class="text-dark">HERRAJES ANDREA</strong>
+          </a></p>
+        
+      </div>
+
+      
+      
+    </div>
+
+  </div>
+  
+</footer>
+<script src="js/loader.js"></script>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
